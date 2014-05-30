6cc11a523c03034a56d331300bef39d9a89dd870
KOKOAR <heyrix@gmail.com>
2014-05-30 16:05:54 +0800

    [iOS] Create a new bar code project.

---
+++ HiBarCode/HiBarCode/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  HiBarCode
+//
+//  Created by Huang Liang on 2014-5-30.
+//  Copyright (c) 2014年 Huang Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HiBarCode/HiBarCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HiBarCode
+//
+//  Created by Huang Liang on 2014-5-30.
+//  Copyright (c) 2014年 Huang Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HTAppDelegate class]));
+    }
+}
